769875f2c9f9ccbbc13c51601624ecfea198ce6b
Abdo Ahmed <ba2662003@gmail.com>
2025-03-02 04:29:51 +0200

    fixed error 2

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,26 +3,26 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-// const sequelize = new Sequelize({
-//     dialect:"postgres",
-//     host:process.env.DB_HOST,
-//     port:+(process.env.DB_PORT as string),
-//     database:process.env.DB_NAME,
-//     username:process.env.DB_USER,
-//     password:process.env.DB_PASSWORD,
-//     logging:false,
-// })
+const sequelize = new Sequelize({
+    dialect:"postgres",
+    host:process.env.DB_HOST,
+    port:+(process.env.DB_PORT as string),
+    database:process.env.DB_NAME,
+    username:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    logging:false,
+})
 
-const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-    logging: false,
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+//     dialect: "postgres",
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         },
+//     },
+//     logging: false,
+// });
 
 export const connectDB = ()=>{
     sequelize.authenticate().then(()=>{
